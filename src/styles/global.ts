c7bb3ce4f6c9ec8a9de946bff954632a8d00d29a
JupiterCoffeee <jvcfialho@gmail.com>
2024-01-16 19:40:45 +0000

    Default theme and global style configured

---
+++ src/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+ * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${props => props.theme['blue-300']};
+    }
+ }
+
+ body {
+    background-color: ${props => props.theme['dark-blue-800']};
+    color: ${props => props.theme['gray-300']};
+    -webkit-font-smoothing: antialiased;
+ }
+
+ body, input, textarea, button {
+    font: 400 1rem 'Nunito', sans-serif;
+    line-height: 1.6;
+ }
+
+ a {
+    color: ${props => props.theme['blue-300']};
+
+        &:hover {
+            text-decoration: underline;
+            transition: text-decoration 200ms;
+        }
+    }
+`;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+    white: '#fff',
+  
+    'dark-blue-200': '#7B96B2',
+    'dark-blue-300': '#7B96B2',
+    'dark-blue-400': '#3A536B',
+    'dark-blue-500': '#1C2F41',
+    'dark-blue-600': '#112131',
+    'dark-blue-700': '#0B1B2B',
+    'dark-blue-800': '#071422',
+    'dark-blue-900': '#040F1A',
+  
+    'gray-100': '#E7EDF4',
+    'gray-200': '#C4D4E3',
+    'gray-300': '#AFC2D4',
+  
+    'blue-300': '#3294F8',
+  } as const
