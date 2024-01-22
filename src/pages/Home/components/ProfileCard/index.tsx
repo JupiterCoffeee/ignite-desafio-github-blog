cedf143d44b8585f662ebab0d31687ce2b604602
JupiterCoffeee <jvcfialho@gmail.com>
2024-01-22 17:27:33 +0000

    HTTPS call to User Github API

--- src/pages/Home/components/ProfileCard/index.tsx
+++ src/pages/Home/components/ProfileCard/index.tsx
@@ -1,41 +1,90 @@
 import { ArrowSquareOut, Buildings, GithubLogo, Users } from "phosphor-react";
 import { ProfileCardHeader, ProfileCardContainer, ProfileCardContentDiv, ProfileCardPictureDiv, ProfileCardStats } from "./style";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface ProfileCardProps {
+    name: string;
+    url: string;
+    login: string;
+    picture: string
+    company: string;
+    followers: number;
+    bio: string;
+}
+
 
 export function ProfileCard() {
+    const [profile, setProfile] = useState<ProfileCardProps>({
+        name: '',
+        url: '',
+        picture: '',
+        login: '',
+        company: '',
+        followers: 0,
+        bio: '',
+    });
+    const apiUrl = 'https://api.github.com';
+
+    const fetchUserInfo = async () => {
+        const username = 'JupiterCoffee';
+        try {
+            const response = await axios.get(`${apiUrl}/users/${username}`)
+            const userData = response.data;
+            const userInfo: ProfileCardProps = {
+                name: userData.name,
+                url: userData.html_url,
+                picture: userData.avatar_url,
+                login: userData.login,
+                company: userData.company,
+                followers: userData.followers,
+                bio: userData.bio,
+            }
+
+            setProfile(userInfo)
+        } catch (error) {
+            console.error('Erro ao buscar usuário:', axios.isAxiosError(error))
+        }
+    };
+
+    useEffect(() => {
+        fetchUserInfo()
+    }, [])
+
+    console.log(profile)
+      
     return (
         <ProfileCardContainer>
             <ProfileCardPictureDiv>
-                <img src="https://avatars.githubusercontent.com/u/133159511?s=400&u=8c2d2c26840eea3500442ed8f0bdb29f3c40b753&v=4" alt=""/>
+                <img src={profile.picture} alt=""/>
             </ProfileCardPictureDiv>
             <ProfileCardContentDiv>
                 <ProfileCardHeader>
                     <div>
                         <span>João Fialho</span>
-                        <a>
+                        <a href={profile.url}>
                             GITHUB
                             <ArrowSquareOut weight="bold"/>
                         </a>
                     </div>
                     <div>
                         <p>
-                            Tristique volutpat pulvinar vel massa, pellentesque egestas. 
-                            Eu viverra massa quam dignissim aenean malesuada suscipit. 
-                            Nunc, volutpat pulvinar vel mass.
+                            {profile.bio}
                         </p>
                     </div>
                 </ProfileCardHeader>
                 <ProfileCardStats>
                     <span>
                         <GithubLogo weight="fill"/>
-                        <p>cameronwll</p>
+                        <p>{profile.login}</p>
                     </span>
                     <span>
                         <Buildings weight="fill"/>
-                        <p>Rocketseat</p>
+                        <p>{profile.company}</p>
                     </span>
                     <span>
                         <Users weight="fill"/>
-                        <p>32 seguidores</p>
+                        <p>{profile.followers} seguidores</p>
                     </span>
                 </ProfileCardStats>
             </ProfileCardContentDiv>
