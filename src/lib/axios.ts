f3c559c3dace5e5ef73988b7b046380078bdcb39
JupiterCoffeee <jvcfialho@gmail.com>
2024-01-24 17:06:00 +0000

    Token updated

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,4 @@ export const api = axios.create({
     baseURL: 'https://api.github.com',
 })
 
-export const token = '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'
+export const token = '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'
