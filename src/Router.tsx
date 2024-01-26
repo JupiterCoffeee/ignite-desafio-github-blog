9e9dafad85cd79a484bd7f17ba116cb50635d179
JupiterCoffeee <jvcfialho@gmail.com>
2024-01-26 16:19:19 +0000

    Dynamic router configured

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,15 +2,33 @@ import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./styles/layouts/DefaultLayout";
 
 import { Home } from "./pages/Home";
+import { Post } from "./pages/Post";
+import { IssueProvider, IssuesContext } from "./context/IssueContext";
+import { useContext } from "react";
 
 export function Router() {
+    const { issues } = useContext(IssuesContext)
+    const issueRoutes = (issues && issues.map(issue => {
+        return issue.number
+    }))
+    console.log(issueRoutes)
+
     return (
-        <>
+        <IssueProvider>
             <Routes>
                 <Route path="/" element={<DefaultLayout />}>
-                    <Route path="/" element={<Home />}/>
+                    <Route path="/" element={<Home />} />
+                    <Route 
+                        path=":id" 
+                        element={
+                        <Post 
+                            issueNumber={
+                                issueRoutes
+                            }/>
+                    }
+                    />
                 </Route>
             </Routes>
-        </>
+        </IssueProvider>
     );
 }
